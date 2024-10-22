be4c26c21ba37f42565e67e06eaac0846a915cdf
Moritz Sanft <58110325+msanft@users.noreply.github.com>
2024-10-22 09:38:32 +0200

    wip

--- fullspeed/solve/Program.cs
+++ fullspeed/solve/Program.cs
@@ -13,30 +13,33 @@ class Program
     static void Init()
     {
         BigInteger p = new BigInteger("c90102faa48f18b5eac1f76bb40a1b9fb0d841712bbe3e5576a7a56976c2baeca47809765283aa078583e1e65172a3fd", 16);
+        Console.WriteLine($"P: {p}");
 
-        BigInteger a = BigInteger.Zero;
-        BigInteger b = BigInteger.Zero;
+        BigInteger a = new BigInteger("a079db08ea2470350c182487b50f7707dd46a58a1d160ff79297dcc9bfad6cfc96a81c4a97564118a40331fe0fc1327f", 16);
+        Console.WriteLine($"A: {a}");
+
+        BigInteger b = new BigInteger("9f939c02a7bd7fc263a4cce416f4c575f28d0c1315c4f0c282fca6709a5f9f7f9c251c9eede9eb1baa31602167fa5380", 16);
+        Console.WriteLine($"B: {b}");
 
         FpCurve curve = new FpCurve(p, a, b, null, null);
 
-        BigInteger bigInt1 = new BigInteger("087b5fe3ae6dcfb0e074b40f6208c8f6de4f4f0679d6933796d3b9bd659704fb85452f041fff14cf0e9aa7e45544f9d8", 16);
-        BigInteger bigInt2 = new BigInteger("127425c1d330ed537663e87459eaa1b1b53edfe305f6a79b184b3180033aab190eb9aa003e02e9dbf6d593c5e3b08182", 16);
+        BigInteger serverX = new BigInteger("b3e5f89f04d49834de312110ae05f0649b3f0bbe2987304fc4ec2f46d6f036f1a897807c4e693e0bb5cd9ac8a8005f06", 16);
+        BigInteger serverY = new BigInteger("85944d98396918741316cd0109929cb706af0cca1eaf378219c5286bdc21e979210390573e3047645e1969bdbcb667eb", 16);
 
-        ECPoint point = curve.CreatePoint(bigInt1, bigInt2);
-        Console.WriteLine($"Point: {point}");
+        ECPoint serverP = curve.CreatePoint(serverX, serverY);
+        Console.WriteLine($"Server P: {serverP}");
 
-        SecureRandom rand = new SecureRandom();
-        Console.WriteLine($"BC Random: {rand}");
+        BigInteger secret = new BigInteger("7ed85751e7131b5eaf5592718bef79a9", 16);
 
-        System.Random sysrand = new System.Random();
-        Console.WriteLine($"System Random: {sysrand}");
+        ECPoint sharedP = serverP.Multiply(secret).Normalize();
+
+        BigInteger sharedX = sharedP.AffineXCoord.ToBigInteger();
+
+        Byte[] sharedXBytes = sharedX.ToByteArray();
+
+        String sharedXHex = Convert.ToHexString(sharedXBytes);
+        Console.WriteLine($"Shared X: {sharedXHex}");
 
         return;
-    }
-
-    static void SomeXORCrypto() {
-        BigInteger foo = new BigInteger("133713371337133713371337133713371337133713371337133713371337133713371337133713371337133713371337", 16);
-
-
     }
 }
--- fullspeed/solve/obj/Release/net8.0/fullspeed.AssemblyInfo.cs
+++ fullspeed/solve/obj/Release/net8.0/fullspeed.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("fullspeed")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+060b943fe50c0b8717d49d1f0a56ec08a3cc76ab")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c95c9d56b3ba57b4f7ef426ae2f753ebd0f416e5")]
 [assembly: System.Reflection.AssemblyProductAttribute("fullspeed")]
 [assembly: System.Reflection.AssemblyTitleAttribute("fullspeed")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
